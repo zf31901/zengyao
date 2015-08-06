8ad6415c3e106cc3389e3eb54e86f7c50d57ad70
123 <123>
2015-08-06 11:16:13 +0800

    测试

--- TSMedicine/Controllers/AskForDonationViewController/CommitAskForViewController.m
+++ TSMedicine/Controllers/AskForDonationViewController/CommitAskForViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "CommitAskForViewController.h"
-
+#import "RegisterViewController.h"
 @interface CommitAskForViewController ()
 @property (weak, nonatomic) IBOutlet X_TableView *tableView;
 
@@ -17,9 +17,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [self UIlable];
+    
     self.title = @"申请捐助";
-    NSArray *titleArr = @[@"姓名",@"身份证",@"地址",@"通讯住址"];
+    NSArray *titleArr = @[@"姓名",@"身份证",@"区域",@"通讯住址"];
     NSMutableArray *arr = [[NSMutableArray alloc]init];
     for (int i = 0; i <titleArr.count; i++) {
         [arr addObject:[@{
@@ -38,20 +39,29 @@
     }
     self.tableView.xDataSource = arr;
 }
+-(void)UIlable{
+    UIButton *btn = [UIButton buttonWithType:0];
+    
+    btn.frame = CGRectMake(320, 0, 40, 120);
+    [btn setTitle:@"提交" forState:0];
+    [btn.titleLabel setFont:[UIFont systemFontOfSize:18]];
+    [btn setTitleColor:[UIColor whiteColor] forState:0];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:btn];
+    [btn addTarget:self action:@selector(nextpageVC) forControlEvents:UIControlEventTouchUpInside];
+    btn.imageEdgeInsets = UIEdgeInsetsMake(0, -8, 0,40);
+    
+    btn.titleEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 0);
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+}
+- (void)nextpageVC
+{
+    RegisterViewController *commitVC = [RegisterViewController new];
+    [self.navigationController pushViewController:commitVC animated:YES];
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
